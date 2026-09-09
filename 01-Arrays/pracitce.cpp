626515f15975c8774b790a214d03bbcd6e36d468
siddharth <siddharthkumar00021@gmail.com>
2026-09-09 22:14:50 +0530

    Refactor practice program to implement two-sum logic using unordered_map

--- 01-Arrays/pracitce.cpp
+++ 01-Arrays/pracitce.cpp
@@ -2,22 +2,28 @@
 #include <vector>
 #include<map>
 #include<set>
-#include<unordered_set>
+#include<unordered_map>
 #include <algorithm>
 using namespace std;
  
 int main(){
     
-    vector<int> v = {5,2,11,7,5};
-    int target = 9;
-    
-    if(v.begin() != v.end()){
-        cout<<"yes";
+   int target = 9;
+   vector<int> v = {2,7,11,15};
+   unordered_map<int,int> u;
+   vector<int> ans;
+
+   for(int i =0;i<v.size();i++){
+    int first = v[i];
+    int second = target - first;
+    if(u.find(second) != u.end()){
+        ans.push_back(u[second]);
+        ans.push_back(i);
     }
-    // for(int i = 0;i<v.size();i++){
-    //     int start = v[0];
-
-
-    // }
+    u[first] = i;
+   }
+   for(int i : ans){
+    cout<<v[i]<<" ";
+   }
     return 0;
 }
